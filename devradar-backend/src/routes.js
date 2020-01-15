823cfeee30cd7a430fec74f90b056c0987d73f72
Marcio Mendes <mmendes.as@gmail.com>
2020-01-14 23:24:20 -0300

    feat: list all devs

--- devradar-backend/src/routes.js
+++ devradar-backend/src/routes.js
@@ -5,5 +5,6 @@ const DevController = require('./controllers/DevController');
 const routes = Router();
 
 routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
 
 module.exports = routes;

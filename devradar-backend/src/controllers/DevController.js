39c297b1db379aa4d42d393316bf16d3724c2576
Marcio Mendes <mmendes.as@gmail.com>
2020-01-14 23:20:28 -0300

    feat: prevent duplicated registration of devs

--- devradar-backend/src/controllers/DevController.js
+++ devradar-backend/src/controllers/DevController.js
@@ -5,25 +5,31 @@ module.exports = {
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
 
-    const response = await axios.get(
-      `http://api.github.com/users/${github_username}`
-    );
+    // check if dev exists
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = response.data;
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    };
+    if (!dev) {
+      const response = await axios.get(
+        `http://api.github.com/users/${github_username}`
+      );
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const { name = login, avatar_url, bio } = response.data;
+      const techsArray = techs.split(',').map(tech => tech.trim());
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
+
+      // register new dev
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return res.json(dev);
   }

a2d8e91806b60a81f1d1a9f523e5eb88ed706e3f
Marcio Mendes <mmendes.as@gmail.com>
2020-01-15 19:28:49 -0300

    feat: starting draw the side form

--- devradar-web/src/App.js
+++ devradar-web/src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+
+import './Sidebar.css';
+
 function App() {
   return (
     <div id="app">
-      <aside></aside>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input
+              type="text"
+              name="github_username"
+              id="github_username"
+              required
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input type="text" name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="text" name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="text" name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main></main>
     </div>
   );

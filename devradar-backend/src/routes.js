2853f5d2f68322631adf6a2033f112dcfe9681e1
Marcio Mendes <mmendes.as@gmail.com>
2020-01-14 23:05:02 -0300

    feat: get github info with axios and register user using mongobd

--- devradar-backend/src/routes.js
+++ devradar-backend/src/routes.js
@@ -1,11 +1,31 @@
 const { Router } = require('express');
+const axios = require('axios');
+
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-  const { github_username } = req.body;
+routes.post('/devs', async (req, res) => {
+  const { github_username, techs } = req.body;
 
-  return res.json({ ok: true });
+  const response = await axios.get(
+    `http://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = response.data;
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  console.log(name, avatar_url, bio);
+
+  return res.json(dev);
 });
 
 module.exports = routes;

743eb35f635993d9ca72dc76c7738083e203dd00
Marcio Mendes <mmendes.as@gmail.com>
2020-01-14 22:54:13 -0300

    feat: add mongoDB integration with first model

--- devradar-backend/src/index.js
+++ devradar-backend/src/index.js
@@ -4,6 +4,15 @@ const routes = require('./routes');
 
 const app = express();
 
+// db connection
+mongoose.connect(
+  'mongodb+srv://omnistack:devradar@omnistack-mm-uyryo.mongodb.net/db_devradar?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
 app.use(express.json());
 app.use(routes);
 
--- devradar-backend/src/routes.js
+++ devradar-backend/src/routes.js
@@ -3,6 +3,8 @@ const { Router } = require('express');
 const routes = Router();
 
 routes.post('/devs', (req, res) => {
+  const { github_username } = req.body;
+
   return res.json({ ok: true });
 });
 

fc9027cec6d144351cbe3dba0cdc179548c285c7
Marcio Mendes <mmendes.as@gmail.com>
2020-01-14 23:43:52 -0300

    feat: add search controller and find by geolocation and techs

--- devradar-backend/src/controllers/DevController.js
+++ devradar-backend/src/controllers/DevController.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
+
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
   async index(req, res) {
@@ -19,7 +21,7 @@ module.exports = {
       );
 
       const { name = login, avatar_url, bio } = response.data;
-      const techsArray = techs.split(',').map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
       const location = {
         type: 'Point',
         coordinates: [longitude, latitude]

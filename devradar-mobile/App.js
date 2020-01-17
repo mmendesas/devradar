52ae36b6e470fab38954095364b3d5b1e5ed528d
Marcio Mendes <mmendes.as@gmail.com>
2020-01-17 00:18:18 -0300

    feat: setting up navigation structure

--- devradar-mobile/App.js
+++ devradar-mobile/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello omnistack</Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0aa',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontWeight: 'bold',
-    color: '#fff',
-    fontSize: 32
-  }
-});

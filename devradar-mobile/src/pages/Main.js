12a886d2b0734f4949467755826ac0a944a0958b
Marcio Mendes <mmendes.as@gmail.com>
2020-01-17 21:00:04 -0300

    feat: adding form components

--- devradar-mobile/src/pages/Main.js
+++ devradar-mobile/src/pages/Main.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import {
   requestPermissionsAsync,
@@ -38,27 +46,41 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -23.5270445, longitude: -46.8204377 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: 'https://avatars0.githubusercontent.com/u/17890831?s=460&v=4'
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -23.5270445, longitude: -46.8204377 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://avatars0.githubusercontent.com/u/17890831?s=460&v=4'
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate('Profile', { github_username: 'mmendesas' });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Marcio Mnedes</Text>
+              <Text style={styles.devBio}>Frontend Developer</Text>
+              <Text style={styles.devTechs}>Reac, ReactNative, Nodejs</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate('Profile', { github_username: 'mmendesas' });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Marcio Mnedes</Text>
-            <Text style={styles.devBio}>Frontend Developer</Text>
-            <Text style={styles.devTechs}>Reac, ReactNative, Nodejs</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -86,6 +108,39 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row'
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#0aa',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15
   }
 });
 
